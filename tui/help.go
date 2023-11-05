057482965d11518498e3e65afec30f1d0685307d
peter wang <wangbo.red@gmail.com>
2023-11-05 10:36:08 +0800

    refactor help view of tui to make code clean

--- tui/help.go
+++ tui/help.go
@@ -3,53 +3,47 @@ package tui
 import (
 	"fmt"
 
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 var content = `
-    <F1>,<Alt>+1   - switch to process view
-    <F2>,<Alt>+2   - switch to system view
-    'g'            - show gernal info (process view only)
-    'c'            - show cpu-relate info
-    'm'            - show memory-relate info
-    'd'            - show disk-relate info
-    'n'            - show network-relate info (process view only)
+	t               - show next sample
+	<Shift>+t       - show preview sample
+	<F1>, <Alt>+1   - switch to process view
+	<F2>, <Alt>+2   - switch to system view
+	'b'             - open Dialog to search specific sample
+
+process view:
+	's'             - sort by specific field
+	'/'             - filter
+	'g'             - show general info
+	'c'             - show cpu info
+	'm'             - show memory info
+	'd'             - show disk info
+
+system view:
+	'c'             - show cpu info
+	'm'             - show memory info
+	'v'             - show vm info
+	'd'             - show disk info
+	'n'             - show networ info
 
 	Type 'ESC' to close
 `
 
 type Help struct {
-	*tview.Box
-	text *tview.TextView
+	*tview.TextView
 }
 
 func NewHelp() *Help {
 
 	help := &Help{
-		Box:  tview.NewBox(),
-		text: tview.NewTextView(),
+		TextView: tview.NewTextView(),
 	}
 
 	help.SetTitle("Help").SetBorder(true).SetTitleAlign(tview.AlignLeft)
 
-	fmt.Fprintf(help.text, content)
+	fmt.Fprintf(help, content)
 
 	return help
-}
-
-func (help *Help) HasFocus() bool {
-	return help.text.HasFocus()
-}
-
-func (help *Help) Focus(delegate func(p tview.Primitive)) {
-	delegate(help.text)
-}
-
-func (help *Help) Draw(screen tcell.Screen) {
-	help.Box.DrawForSubclass(screen, help)
-	x, y, width, height := help.Box.GetInnerRect()
-
-	help.text.SetRect(x, y, width, height)
-	help.text.Draw(screen)
 }
