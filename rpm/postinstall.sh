e8e72a5c30a7fc0ea3e0f720cffdc79df70552b4
peter wang <wangbo.red@gmail.com>
2024-06-30 22:36:50 +0800

    restart etop service after rpm upgrade

--- rpm/postinstall.sh
+++ rpm/postinstall.sh
@@ -2,3 +2,8 @@ if [ $1 -eq 1 ] && [ -x /usr/bin/systemctl ]; then
     # Initial installation
     systemctl enable --now etop || :
 fi
+
+if [ $1 -eq 2 ] && [ -x /usr/bin/systemctl ]; then
+    # Package upgrade
+    systemctl restart etop || :
+fi
