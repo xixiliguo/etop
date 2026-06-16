d3232424104a1ac2d584f479d4c47a1772c4ccdd
peter wang <wangbo.red@gmail.com>
2026-06-17 06:58:02 +0800

    fix nstgid format

--- model/field.go
+++ model/field.go
@@ -95,10 +95,12 @@ func (f Field) Render(value any) string {
 	case []uint64:
 		buf = append(buf, '[')
 		for i := range v {
+			if i != 0 {
+				buf = append(buf, ' ')
+			}
 			buf = strconv.AppendUint(buf, v[i], 10)
-			buf = append(buf, ' ')
-			buf[len(buf)-1] = ']'
 		}
+		buf = append(buf, ']')
 	default:
 		return fmt.Sprintf("%T is unknown type", v)
 	}
